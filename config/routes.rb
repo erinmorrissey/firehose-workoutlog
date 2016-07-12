a76b0fc6aa1aceb22cc98e256b06cfc1894bb15e
Erin Morrissey <erinmorrissey@gmail.com>
2016-07-12 19:25:44 +0000

    Create 'workouts' controller & create index view

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Workoutlog::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
+  root 'workouts#index'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
