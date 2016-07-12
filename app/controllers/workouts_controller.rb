43bc25f43ae7be7f66c06cf034c099e7d0667fbf
Erin Morrissey <erinmorrissey@gmail.com>
2016-07-12 21:58:23 +0000

    Upon form submit, workout form details are stored in the DB and user is redirected to homepage

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -8,4 +8,15 @@ class WorkoutsController < ApplicationController
     @workout = Workout.new
   end
 
+  def create
+    Workout.create(workout_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def workout_params
+    params.require(:workout).permit(:date, :activity, :mood, :duration)
+  end
+
 end
