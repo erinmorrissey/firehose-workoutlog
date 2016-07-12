6aefd08bf50e95e0bedaedc5ebd2a681880fbccd
Erin Morrissey <erinmorrissey@gmail.com>
2016-07-12 18:36:31 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Workoutlog::Application.config.secret_key_base = '89f41361d5ec336300a3b2b7308643e882c8ac861cee66bc4351eccf1acd0c89df81f34e44f7491930cf5b94acd7e01b4c74f826b945cb6d05fc2a9acd970a0a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Workoutlog::Application.config.session_store :cookie_store, key: '_workoutlog_session'
